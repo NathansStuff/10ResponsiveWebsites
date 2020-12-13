464a0f27f69e534f9f4c88562df1fcd05221cbd3
Nathanstuff <n.odonnell@hotmail.com>
2020-12-13 23:57:25 +1100

    foods section 1

---
+++ Foods/script.js
@@ -0,0 +1,13 @@
+const icons = document.querySelectorAll('.section-1-icons i')
+let i = 1
+setInterval(() => {
+    i++
+    const icon = document.querySelector('.section-1-icons .active')
+    icon.classList.remove('active')
+    if (i > icons.length) {
+        icons[0].classList.add('active')
+        i = 1
+    } else {
+        icon.nextElementSibling.classList.add('active')
+    }
+},4000)
